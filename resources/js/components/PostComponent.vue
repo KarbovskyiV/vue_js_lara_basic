c3ebf601f46fe0c3b754585fc15e92473ada7db4
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-25 11:36:33 +0200

    create variables and use it

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <SinglePostComponent></SinglePostComponent>
+        <div>Name: {{ name }}</div>
     </div>
 </template>
 
@@ -9,6 +10,13 @@ import SinglePostComponent from "./SinglePostComponent.vue";
 
 export default {
     name: "PostComponent",
+
+    data() {
+        return {
+            name: 'Test',
+            age: 22,
+        }
+    },
 
     components: {
         SinglePostComponent,
