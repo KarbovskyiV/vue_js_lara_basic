ec2654b74e6e1f6f7eaac04939052261a565d485
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-26 16:00:50 +0200

    use create person component

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,29 +1,31 @@
 <template>
     <div>
+        <CreateComponent></CreateComponent>
         <SinglePostComponent></SinglePostComponent>
-        <table class="table">
-            <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Name</th>
-                <th scope="col">Age</th>
-                <th scope="col">Job</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="person in persons">
-                <th scope="row">{{ person.id }}</th>
-                <td>{{ person.name }}</td>
-                <td>{{ person.age }}</td>
-                <td>{{ person.job }}</td>
-            </tr>
-            </tbody>
-        </table>
+<!--        <table class="table">-->
+<!--            <thead>-->
+<!--            <tr>-->
+<!--                <th scope="col">#</th>-->
+<!--                <th scope="col">Name</th>-->
+<!--                <th scope="col">Age</th>-->
+<!--                <th scope="col">Job</th>-->
+<!--            </tr>-->
+<!--            </thead>-->
+<!--            <tbody>-->
+<!--            <tr v-for="person in persons">-->
+<!--                <th scope="row">{{ person.id }}</th>-->
+<!--                <td>{{ person.name }}</td>-->
+<!--                <td>{{ person.age }}</td>-->
+<!--                <td>{{ person.job }}</td>-->
+<!--            </tr>-->
+<!--            </tbody>-->
+<!--        </table>-->
     </div>
 </template>
 
 <script>
 import SinglePostComponent from "./SinglePostComponent.vue";
+import CreateComponent from "./CreateComponent.vue";
 
 export default {
     name: "PostComponent",
@@ -35,24 +37,16 @@ export default {
     },
 
     mounted() {
-        this.getPersons()
+
     },
 
     methods: {
-        getPersons() {
-            axios.get('/persons')
-                .then(res => {
-                    this.persons = res.data
-                }).catch(error => {
 
-            }).finally({
-
-            })
-        }
     },
 
     components: {
         SinglePostComponent,
+        CreateComponent
     }
 }
 </script>
