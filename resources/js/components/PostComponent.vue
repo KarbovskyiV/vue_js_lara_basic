9e67a4865226462ffe93e88eafbc64f104b11e99
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-26 13:42:01 +0200

    use axios with get posts

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,24 +1,6 @@
 <template>
     <div>
         <SinglePostComponent></SinglePostComponent>
-        <table class="table">
-            <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Name</th>
-                <th scope="col">Age</th>
-                <th scope="col">Job</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="person in personsAgeMoreTwenty">
-                <th scope="row">{{ person.id }}</th>
-                <td>{{ person.name }}</td>
-                <td>{{ person.age }}</td>
-                <td>{{ person.job }}</td>
-            </tr>
-            </tbody>
-        </table>
     </div>
 </template>
 
@@ -30,51 +12,25 @@ export default {
 
     data() {
         return {
-            persons: [
-                {
-                    id: 1,
-                    name: 'Name',
-                    age: 22,
-                    job: 'coach',
-                },
-                {
-                    id: 2,
-                    name: 'Name 2',
-                    age: 23,
-                    job: 'second coach',
-                },
-                {
-                    id: 3,
-                    name: 'Name 3',
-                    age: 24,
-                    job: 'third coach',
-                },
-                {
-                    id: 4,
-                    name: 'Name 4',
-                    age: 19,
-                    job: 'fourth coach',
-                },
-                {
-                    id: 5,
-                    name: 'Name 5',
-                    age: 26,
-                    job: 'fifth coach',
-                },
-            ]
+
         }
+    },
+
+    mounted() {
+        this.getPosts()
     },
 
     methods: {
-
+        getPosts() {
+            axios.get('/posts')
+                .then(function (data) {
+                    console.log(data);
+                })
+        }
     },
 
     computed: {
-        personsAgeMoreTwenty() {
-            return this.persons.filter(function (person) {
-                return person.age > 20
-            })
-        }
+
     },
 
     components: {
