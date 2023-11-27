972809c9cb5bdd148fa97f60f8b0e59fa9608638
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-27 22:13:10 +0200

    switch off all routes form laravel

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/{page}', \App\Http\Controllers\IndexController::class)->where('page', '.*');
