56acf48e30acd308eed3cb63a66732620d12e17c
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-27 17:31:23 +0200

    delete trash

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -19,13 +19,9 @@ export default {
     },
 
     mounted() {
-        console.log(this.$refs.index.indexLog());
     },
 
     methods: {
-        parentLog() {
-            console.log('this is parent component');
-        }
     },
 
     components: {
