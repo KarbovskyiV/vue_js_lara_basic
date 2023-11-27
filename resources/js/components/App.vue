6956a1a5970673545708015fd3533922d477a0cc
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-27 23:21:56 +0200

    delete welcome route

---
+++ resources/js/components/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <router-link :to="{ name: 'person.index' }">People</router-link>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Index",
+
+    data() {
+        return {
+
+        }
+    },
+
+    mounted() {
+
+    },
+}
+</script>
+
+
+<style scoped>
+
+</style>
