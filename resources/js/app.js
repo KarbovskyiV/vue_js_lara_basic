facfa9d53caa5b88d16610c96fb7e3621c5d8e19
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2023-11-27 22:13:51 +0200

    connect router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,10 @@
 import './bootstrap';
+import router from "./router.js";
 import {createApp} from 'vue';
 
 const app = createApp({});
+
+app.use(router)
 
 Object.entries(import.meta.glob('./**/*.vue', {eager: true})).forEach(([path, definition]) => {
     app.component(path.split('/').pop().replace(/\.\w+$/, ''), definition.default);
